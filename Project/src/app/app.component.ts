98ba6273b98389aba935a8b97d7fef58ef68275e
Jan Patel <jaanpatel2004@gmail.com>
2023-08-04 11:35:03 -0400

    Removed user files cause it had errors

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './shared/services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  userSignedin = false;
+
+  constructor(public afauth: AngularFireAuth){}
+
+  signIn(){
+    this.userSignedin = true;
+  }
+
+  signOut(){
+    this.userSignedin = false;
+  }
 
   title = 'Project';
 
